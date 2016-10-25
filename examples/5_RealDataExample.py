357c72e5d262a1a58e36a843386c3bc75a21b55c
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2016-10-25 11:09:49 +0200

    test sans s_data

--- examples/5_RealDataExample.py
+++ examples/5_RealDataExample.py
@@ -9,5 +9,6 @@ data = np.load('RealDataExample.npz')
 s_data = data['beta']
 s_xyz = data['xyz']
 
-vb = vbrain(s_xyz=s_xyz, s_data=s_data, cmap_vmin=-1, cmap_vmax=1, cmap='jet')
+print s_data
+vb = vbrain(s_xyz=s_xyz, cmap_vmin=-1, cmap_vmax=1, cmap='jet')
 vb.show()
